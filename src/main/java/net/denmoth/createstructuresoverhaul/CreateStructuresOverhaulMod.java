acd8da4cfe42d179098206bae113d7dd5f4fc29f
Denmoth <denmoth8871top@gmail.com>
2026-01-15 18:10:40 +0200

    Clean up, and optimization

--- src/main/java/net/denmoth/createstructuresoverhaul/CreateStructuresOverhaulMod.java
+++ src/main/java/net/denmoth/createstructuresoverhaul/CreateStructuresOverhaulMod.java
@@ -1,16 +1,11 @@
 package net.denmoth.createstructuresoverhaul;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(CreateStructuresOverhaulMod.MODID)
 public class CreateStructuresOverhaulMod {
     public static final String MODID = "create_structures_overhaul";
-    public static final Logger LOGGER = LogManager.getLogger();
 
     public CreateStructuresOverhaulMod() {
-        LOGGER.info("[CSO] Loaded Create: Structures Overhaul");
     }
 }
